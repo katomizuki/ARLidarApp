bf287073ce8544b4b5497a4bcfaff0f09573f985
Katopan <76249494+katomizuki@users.noreply.github.com>
2023-04-13 21:38:15 +0900

    mvp

---
+++ Assets/Scripts/View/MainARView.cs
@@ -0,0 +1,16 @@
+using VContainer;
+using UnityEngine;
+
+public class MainARView : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
