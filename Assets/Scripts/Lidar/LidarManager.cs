1bc5bd6057c4dc248f7a20db248afd429a1ffe20
Katopan <76249494+katomizuki@users.noreply.github.com>
2023-04-27 08:17:44 +0900

    fix

--- Assets/Scripts/Lidar/LidarManager.cs
+++ Assets/Scripts/Lidar/LidarManager.cs
@@ -1,9 +1,37 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 namespace Lidar
 {
+    [RequireComponent(typeof(ARMeshManager))]
     public class LidarManager : MonoBehaviour
     {
+        private ARMeshManager _arMeshManager;
+
+        private void Awake()
+        {
+            _arMeshManager = GetComponent<ARMeshManager>();
+           _arMeshManager.meshesChanged += OnMeshesChanged;  
+        }
+        
+        private void OnMeshesChanged(ARMeshesChangedEventArgs args)
+        {
+            foreach (var meshFilter in args.added)
+            {
+            }
+            
+            foreach (var mesh in args.updated)
+            {
+                Debug.Log($"updated {mesh.mesh.uv.Length}");
+            }
+
+            foreach (var mesh in args.removed)
+            {
+                Debug.Log($"removed {mesh.mesh.triangles.Length}");
+            }
+        }
     }  
 }
 
